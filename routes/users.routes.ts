0bfde342b4034fdc6fe649c9251595dbda02e504
Andres Farfan <a22310560@ceti.mx>
2025-04-02 09:52:34 -0600

    Agregado uso básico de JWT

--- routes/users.routes.ts
+++ routes/users.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { fetchInfo, pong } from "../controllers/users.controller";
+import { authenticateUser, fetchInfo, pong } from "../controllers/users.controller";
 
 const router: Router = Router();
 
 router.get('/ping', pong);
+
+// TODO: Eliminar endpoint de prueba de BD.
 router.get("/user_info", fetchInfo);
+
+// TODO: Cambiar a endpoint de producción para autenticación.
+router.get("/test_login", authenticateUser);
 
 export default router;
