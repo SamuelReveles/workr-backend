c6c0c669df8be078bec3512f04994764c8c78f24
Andres Farfan <a22310560@ceti.mx>
2025-04-04 17:39:49 -0600

    Simplificado controlador de usuarios
    - Se eliminó una ruta temporal de prueba de BD
    - Se movieron las funciones de JWT a su propio archivo

--- controllers/users.controller.ts
+++ controllers/users.controller.ts
@@ -1,22 +1,10 @@
 import { RowDataPacket } from "mysql2";
-import { executeQuery } from "../database/connection";
-import JWT, { SignOptions } from "jsonwebtoken";
 import User from "../models/User";
 
-let secretKey;
+import { generateJWT, verifyJWT } from "../helpers/jwt";
 
 export const pong = (req, res) => {
     return res.status(200).json('pong');
-}
-
-// TODO: Eliminar este endpoint en desarrollo posterior.
-// Endpoint de prueba de BD.
-export const fetchInfo = async (req, res) => {
-    const query = "SELECT * FROM Users"
-    const results: RowDataPacket[] = await executeQuery(query, null)
-    console.log(results)
-
-    return res.status(200).json("correct request");
 }
 
 // TODO: Crear endpoint de producción con validación de contraseña.
@@ -37,59 +25,8 @@ export const authenticateUser = (req, res) => {
     return res.status(200).json({ token: userToken });
 }
 
-// Inicialización de la clave privada usada en JWT.
-function init() {
-    secretKey = process.env.JWT_SECRET_KEY;
-}
 
-/**
- * Crea un Json Web Token para un usuario especificado.
- * @param user Usuario autenticado.
- * @returns Token generado para almacenamiento en el cliente.
- */
-function generateJWT(user: User) {
-    // Inicializa la clave privada usada en JWT si aún no se ha obtenido.
-    if(!secretKey) {
-        init();
-    }
 
-    // Datos del usuario a incluir en el token.
-    const payload = {
-        userId: user.id,
-        userName: user.name,
-    };
     
-    // Configuración del token, por ejemplo su expiración.
-    const options: SignOptions = {
-        expiresIn: "1h",
-    };
-    
-    // Generación y firmado del token.
-    const token = JWT.sign(payload, secretKey, options);
-    return token;
-}
 
-/**
- * Verifica un token identificador de un usuario.
- * @param token Token a verificar.
- * @returns Payload del token verificado en caso de ser válido,
- * o null en caso de ser inválido.
- */
-function verifyJWT(token: string) {
-    // Inicializa la clave privada usada en JWT si aún no se ha obtenido.
-    if(!secretKey) {
-        init();
-    }
 
-    // Se intenta decodificar el token con la clave privada y se retorna
-    // su payload en caso de ser validado.
-    try {
-        const decoded = JWT.verify(token, secretKey);
-        return decoded;
-    }
-    // Si el token es inválido se atrapa su error y se devuelve null.
-    catch (error) {
-        console.error("JWT verification failed:\n", error.message)
-        return null;
-    }
-}
--- routes/users.routes.ts
+++ routes/users.routes.ts
@@ -1,12 +1,7 @@
 import { Router } from "express";
-import { authenticateUser, fetchInfo, pong } from "../controllers/users.controller";
+import { authenticateUser } from "../controllers/users.controller";
 
 const router: Router = Router();
-
-router.get('/ping', pong);
-
-// TODO: Eliminar endpoint de prueba de BD.
-router.get("/user_info", fetchInfo);
 
 // TODO: Cambiar a endpoint de producción para autenticación.
 router.get("/test_login", authenticateUser);
