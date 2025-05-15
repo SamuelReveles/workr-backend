b18798cdfaf5d95eeca99ca6e5448f3edbd7f4a5
Andres Farfan <a22310560@ceti.mx>
2025-05-14 19:54:02 -0600

    Agregada id referencia para oficina virtual en login

--- dist/models/Auth.js
+++ dist/models/Auth.js
@@ -25,9 +25,17 @@ class Auth {
             // Primero se intenta hacer un login como usuario.
             let queryResults = yield (0, connection_1.executeQuery)("SELECT id, hashed_password FROM Users WHERE email = ?", [email]);
             let loginType = "";
+            let virtualOfficeCompanyId = "";
             // Si el email coincide con un registro de usuario, se entiende un tipo de login de usuario.
             if (queryResults.length != 0) {
                 loginType = "user";
+                // Se determina si el usuario trabaja para una empresa,
+                // si es así se recupera el id de la empresa para acceder a la oficina virtual,
+                // si no, se devolverá una cadena vacía por defecto en el campo de referencia.
+                let employeeCompanyResults = yield (0, connection_1.executeQuery)("SELECT company_id FROM Employees WHERE user_id = ?", queryResults[0]["id"]);
+                if (employeeCompanyResults.length > 0) {
+                    virtualOfficeCompanyId = employeeCompanyResults[0]["company_id"];
+                }
             }
             else {
                 // Si el email no coincide con un usuario, se intenta un login de empresa.
@@ -35,6 +43,9 @@ class Auth {
                 // Si el email coincide con un registro de empresa, se entiende un tipo de login de empresa.
                 if (queryResults.length != 0) {
                     loginType = "company";
+                    // Se incluye el id de la empresa en el campo de
+                    // referencia para la oficina virtual.
+                    virtualOfficeCompanyId = queryResults[0]["id"];
                 }
                 // Si el email no coincidió de nuevo, se rechaza la promesa de login.
                 else {
@@ -47,7 +58,7 @@ class Auth {
             // creando un jwt de autenticación.
             if ((0, encryption_1.isPasswordEqualToStored)(password, storedPassword)) {
                 const jwt = (0, jwt_1.generateJWT)(queryResults[0]["id"], loginType);
-                return Promise.resolve({ jwt, id: queryResults[0]["id"], loginType });
+                return Promise.resolve({ jwt, id: queryResults[0]["id"], loginType, virtualOfficeCompanyId });
             }
             // Si las contraseñas no coinciden, se rechaza la promesa de login.
             else {
--- models/Auth.ts
+++ models/Auth.ts
@@ -18,10 +18,22 @@ class Auth {
       [email]
     );
     let loginType = "";
+    let virtualOfficeCompanyId = "";
 
     // Si el email coincide con un registro de usuario, se entiende un tipo de login de usuario.
     if (queryResults.length != 0) {
       loginType = "user";
+
+      // Se determina si el usuario trabaja para una empresa,
+      // si es así se recupera el id de la empresa para acceder a la oficina virtual,
+      // si no, se devolverá una cadena vacía por defecto en el campo de referencia.
+      let employeeCompanyResults = await executeQuery(
+        "SELECT company_id FROM Employees WHERE user_id = ?",
+        queryResults[0]["id"]
+      );
+      if (employeeCompanyResults.length > 0) {
+        virtualOfficeCompanyId = employeeCompanyResults[0]["company_id"];
+      }
     }
     else {
       // Si el email no coincide con un usuario, se intenta un login de empresa.
@@ -33,6 +45,10 @@ class Auth {
       // Si el email coincide con un registro de empresa, se entiende un tipo de login de empresa.
       if (queryResults.length != 0) {
         loginType = "company";
+
+        // Se incluye el id de la empresa en el campo de
+        // referencia para la oficina virtual.
+        virtualOfficeCompanyId = queryResults[0]["id"];
       }
       // Si el email no coincidió de nuevo, se rechaza la promesa de login.
       else {
@@ -48,7 +64,7 @@ class Auth {
     // creando un jwt de autenticación.
     if (isPasswordEqualToStored(password, storedPassword)) {
       const jwt = generateJWT(queryResults[0]["id"], loginType);
-      return Promise.resolve({ jwt, id: queryResults[0]["id"], loginType });
+      return Promise.resolve({ jwt, id: queryResults[0]["id"], loginType, virtualOfficeCompanyId });
     }
     // Si las contraseñas no coinciden, se rechaza la promesa de login.
     else {
